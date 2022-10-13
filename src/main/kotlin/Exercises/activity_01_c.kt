12350bb6e2701b446f77134654f6e15cfcee1293
romie quisido <rome.quisido@gmail.com>
2022-10-13 23:20:02 +0800

    added package exercise added activity_01_a.kt added activity_01_b.kt added activity_01_c.kt

--- src/main/kotlin/Exercises/activity_01_c.kt
+++ src/main/kotlin/Exercises/activity_01_c.kt
@@ -1,48 +1,67 @@
 package Exercises
 
+import java.util.Date
+
 fun main() {
     /*You are tasked to automate an inventory system for a grocery.
     Identify the items that can be bought in a grocery store.
     After listing the different items, identify the characteristics of the items.
     You are also tasked to group the items in categories to help manage the grocery.
     Use the proper data types. */
-
     //var beginningInventory: Int? = null
-    val cannedGoods = 100
-    var packsOfNoodles = 150
-    var bathSoap = 80
-    var detergentBar = 60
-    var dozenOfShampoo = 10
-    var dozenOfConditioner = 10
-    var packsOfOil = 10
-    var packsOfSoySauce = 10
-    var loafOfBread = 6
-    var packOfCigarretes = 20
-    var bottleOfmineralWater = 50
-    var packsOfCandies = 20
 
+
+    var meatAndFish: String? = null
+    var dairyProducts: String? = null
+    var vegetablesAndFruits: String? = null
+    var froozenProducts: String? = null
+    var breadAndSpreads: String? = null
+    var driedGoods: String? = null
+    var snacks: String? = null
+    var careProducts: String? = null
+    var condiments: String? = null
+    var beverages: String? = null
+    var wines: String? = null
+    var laundryProducts: String? = null
+
+    var firstIn: Date? = null
+    var firstOut: Date? = null
+
+    var beginningInventory: Int? = null
     var endingInventory: Int? = null
-    var cannedGoods1= 60
-    var packsOfNoodles1= 100
-    var bathSoap1= 40
-    var detergentBar1= 40
-    var dozenOfShampoo1= 5
-    var dozenOfConditioner1= 5
-    var packsOfOil1= 5
-    var packsOfSoySauce1= 5
-    var loafOfBread1= 2
-    var packOfCigarretes1= 10
-    var bottleOfmineralWater1= 25
-    var packsOfCandies1= 20
-    var result: Int = 0
 
-    println("Total #of  canned goods = $cannedGoods")
-    result = 100 - 60
-    println("Remaining balance = $result")
+    var totalSales: Double? = null
+    var lessCapital: Double? = null
+    var implicitCapital: Double? = null
+    var explicitCapital: Double? = null
+    var lessExpenses: Double? = null
+    var totalProfit: Double? = null
+    var revenueIncrease: Boolean? = null
+    var revenueDecrease: Boolean? = null
 
-    println("Enter Beginning Inventory = $packsOfNoodles")
-    result = 150 - 100
-    println("Remaining balance = $result")
+//
+//    var endingInventory: Int? = null
+//    var cannedGoods1= 60
+//    var packsOfNoodles1= 100
+//    var bathSoap1= 40
+//    var detergentBar1= 40
+//    var dozenOfShampoo1= 5
+//    var dozenOfConditioner1= 5
+//    var packsOfOil1= 5
+//    var packsOfSoySauce1= 5
+//    var loafOfBread1= 2
+//    var packOfCigarretes1= 10
+//    var bottleOfmineralWater1= 25
+//    var packsOfCandies1= 20
+//    var result: Int = 0
+//
+//    println("Total #of  canned goods = $cannedGoods")
+//    result = 100 - 60
+//    println("Remaining balance = $result")
+//
+//    println("Enter Beginning Inventory = $packsOfNoodles")
+//    result = 150 - 100
+//    println("Remaining balance = $result")
 
 }
 
