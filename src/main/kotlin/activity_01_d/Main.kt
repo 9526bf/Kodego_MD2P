4ef04eeb73907d32207c2aa598582707ec2d7073
romie quisido <rome.quisido@gmail.com>
2022-10-19 20:47:59 +0800

    added package exercise added activity_01_a.kt added activity_01_b.kt added activity_01_c.kt added activity_01_d.kt

--- src/main/kotlin/activity_01_d/Main.kt
+++ src/main/kotlin/activity_01_d/Main.kt
@@ -3,7 +3,10 @@ package activity_01_d
 import mu.KotlinLogging
 
 lateinit var dogBreeds : Array<String>
-val logger = KotlinLogging.logger{}
+private val logger = KotlinLogging.logger{}
+
+//fun arrayOfNulls() {
+
 
 fun main() {
 
@@ -13,75 +16,98 @@ fun main() {
 //    The total of the 5  input will be divided by the answer in the second question input.
 //    Note: Error checking must be done.*/
 
-    logger.info("INPUT 5 MONETARY AMOUNT")
+    var userInput: String?
+    var amount1: Double = 0.0
+    var amount2: Double = 0.0
+    var amount3: Double = 0.0
+    var amount4: Double = 0.0
+    var amount5: Double = 0.0
+    var divisor: Int = 0
+    var sum: Double = 0.0
+    var quotient: Double
+    var valid = false
 
-    logger.info("Please enter first amount:")
-    var amount1: Double? = readLine()?.toDouble()
-    logger.info("Please enter second amount:")
-    var amount2: Double? = readLine()?.toDouble()
-    logger.info("Please enter third amount:")
-    var amount3: Double? = readLine()?.toDouble()
-    logger.info("Please enter fourth amount:")
-    var amount4: Double? = readLine()?.toDouble()
-    logger.info("Please enter fifth amount:")
-    var amount5: Double? = readLine()?.toDouble()
-    logger.info("Divide the value by how many?")
-    var userInput: Int? = readLine()?.toInt()
-    logger.info("Result: $userInput")
-    var numberToDivide: Int? = readLine()?.toInt()
+    do {
+        logger.info { "Enter amount 1:" }
+        userInput = readLine().toString()
+        if (userInput.toDoubleOrNull() == null) {
+            logger.error("Please enter a valid amount:")
 
+        } else {
+            amount1 = userInput.toDouble()
+            valid = true
+        }
+    } while (!valid)
+    valid = false
 
+    do {
+        logger.info { "Enter amount 2:" }
+        userInput = readLine().toString()
+        if (userInput.toDoubleOrNull() == null) {
+            logger.error("Please enter a valid amount:")
 
-    if (amount1 == null) {
-        do {
-            logger.error { "Please enter the correct input!" }
-        } while (amount1 == null)
-    }
+        } else {
+            amount2 = userInput.toDouble()
+            valid = true
+        }
+    } while (!valid)
+    valid = false
 
-    if (amount2 == null) {
-        do {
-            logger.error { "Please enter the correct input!" }
-        } while (amount2 == null)
-    }
+    do {
+        logger.info { "Enter amount 3:" }
+        userInput = readLine().toString()
+        if (userInput.toDoubleOrNull() == null) {
+            logger.error("Please enter a valid amount:")
 
-    if (amount3 == null) {
-        do {
-            logger.error { "Please enter the correct input!" }
-        } while (amount3 == null)
-    }
+        } else {
+            amount3 = userInput.toDouble()
+            valid = true
+        }
+    } while (!valid)
+    valid = false
 
-    if (amount4 == null) {
-        do {
-            logger.error { "Please enter the correct input!" }
-        } while (amount4 == null)
-    }
+    do {
+        logger.info { "Enter amount 4:" }
+        userInput = readLine().toString()
+        if (userInput.toDoubleOrNull() == null) {
+            logger.error("Please enter a valid amount:")
 
-    if (amount5 == null) {
-        do {
-            logger.error { "Please enter the correct input!" }
-        } while (amount5 == null)
-    }
+        } else {
+            amount4 = userInput.toDouble()
+            valid = true
+        }
+    } while (!valid)
+    valid = false
 
-    if (userInput == null) {
-        do {
-            logger.error { "Please enter the correct input!" }
-        } while (userInput == null)
-    }
-    if (userInput == null) {
-        do {
-            logger.info { "Result : $numberToDivide" }
-        } while (userInput == null)
-    }
+    do {
+        logger.info { "Enter amount 5:" }
+        userInput = readLine().toString()
+        if (userInput.toDoubleOrNull() == null) {
+            logger.error("Please enter a valid amount:")
 
+        } else {
+            amount5 = userInput.toDouble()
+            valid = true
+        }
+    } while (!valid)
+    sum = amount1 + amount2 + amount3 + amount4 + amount5
+    logger.info { "Total amount = $sum" }
 
-    if (numberToDivide == null) {
+    valid = false
+    do{
+        logger.info { "Divide the the total amount by how many?" }
+        userInput = readLine().toString()
+        if (userInput.toIntOrNull() == null){
+            logger.error { "Please enter a valid number:" }
+        }else{
+            divisor = userInput.toInt()
+            valid = true
+        }
+    }while (!valid)
 
-//        do {
+    quotient = sum / divisor.toDouble()
+    logger.info { "Quotient = $quotient" }
 
-            logger.info { "Result = $amount1 + $amount2 + $amount3 + $amount4 + $amount5 / $numberToDivide" }
-//        }while ("result = null")
-
-    }
 }
 
 
@@ -91,38 +117,16 @@ fun main() {
 
 
 
-//    } while (userInput == null)
-//        logger.info { "Result : $result" }
 
 
 
 
-//
-//        if (amount2 != null) {
-//            logger.info {"Result : $result"}
-//
-//            if (amount3 != null) {
-//                logger.info {"Result : $result"}
-//
-//                if (amount4 != null) {
-//                    logger.info {"Result : $result"}
-//
-//                    if (amount5 != null) {
-//                        logger.info {"Result : $result"}
-//
-//                        if (userInput != null) {
-//                            logger.info { "Result : $userInput" }
-//
-//                        }else {logger.info { "Result : $result" }
-//
-//                                }
-//
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
+
+
+
+
+
+
 
 
 
