d9bea8f43c5b690d65138c8fc1a66c5bbf22d1b7
romie quisido <rome.quisido@gmail.com>
2022-10-20 20:08:15 +0800

    added package exercise added activity_01_a.kt added activity_01_b.kt added activity_01_c.kt added activity_01_d.kt added activity_01_e.kt added activity_01_f.kt

---
+++ src/main/kotlin/activity_01_f/Main.kt
@@ -0,0 +1,32 @@
+package activity_01_f
+
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger{}
+
+fun main() {
+    /*Create an application that will accept 2 string inputs.
+
+Your application will print all unique characters in both Strings.
+
+Scope : String/Loops     */
+
+    val inputString1: String? = "Kotlin Programming Language"
+    val inputString2: String? = "Android Studio"
+    var characterInput: MutableList<Char> = mutableListOf<Char>('a', 'b', 'c')
+
+    if (inputString1 != null) {
+        for (inputString1 in inputString1)
+            logger.info { "Unique character in String 1 is $inputString1" }
+    }
+
+    if (inputString2 != null) {
+        for (inputString2 in inputString2)
+            logger.info { "Unique character in String 2 is $inputString2" }
+    }
+
+}
+
+
+
+
